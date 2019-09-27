7ae4ecafb4e28e20015e10e5d55f615dfa3a6a26
MansiNegi27 <mansinegi14091998@gmail.com>
2019-09-27 11:32:41 +0530

    changed structure

---
+++ routes/login.js
@@ -0,0 +1,53 @@
+const router = require('express').Router();
+const User = require('../models/user.model');
+const Issue = require('../models/issue.model');
+
+router.route('/').get((req, res) => {
+  const email  = req.headers.email;
+  const password = req.headers.password;
+  let finalObjectToReturn = {};
+
+User.find({$and: [{email:email}, {password:password}]},function(err,data){
+    if(!err && data)
+    {
+        finalObjectToReturn.username = data[0].username;
+        finalObjectToReturn.email = data[0].email;
+        finalObjectToReturn.password = data[0].password;
+        Issue.find({faculty : data[0].username},function(err,data) {
+          if(!err)
+          {
+              finalObjectToReturn.issue = data;
+              res.send(JSON.stringify(finalObjectToReturn));
+           }else{
+            console.log(err);
+            res.send(JSON('Error: '+err));
+          }
+        })
+    }else{
+      console.log(err);
+      res.send(JSON('Error: '+err));
+    }
+});
+
+});
+
+//{faculty:user.username}
+//console.log(issue);
+  module.exports = router;
+
+
+  //now verify this and check whatever you want
+  //and keep modifying the object, jaise if you get the department , do :
+
+  //finalObjectToReturn.department = department;
+
+  //Here will be the object you want to send to front end
+  //finalObjectToReturn.Juhi = "This is gonna be okay";
+  //finalObjectToReturn.email = email;
+  //finalObjectToReturn.password = password;
+  //return your response stuff too, just aise hi
+
+//check this in frontend ka console.log(); Ctrl+Shift+I
+//this is will show what I get in front end
+
+  //res.json(finalObjectToReturn);
