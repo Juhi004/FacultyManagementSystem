9edc7b7cf5b189d8f67c27b6bfb163b7408ccd52
Juhi004 <juhiagarwalsy@gmail.com>
2019-10-08 20:54:19 +0530

    Added HOD remarks in DB

--- models/issue.model.js
+++ models/issue.model.js
@@ -6,9 +6,11 @@ const issueSchema = new Schema({
   department: {type: String, required: true},
   facultyName: {type: String, ref:'User', required: true},
   subject: {type: String, required: true, trim: true},
+  class: {type: String, required: true, trim: true},
   time : {type: String,required : true},
   date: {type: String, required: true},
-  remarks: {type: String},
+  deanRemarks: {type: String},
+  hodRemarks: {type: String},
   status: {type: String }
 },{
   timestamps: true,
